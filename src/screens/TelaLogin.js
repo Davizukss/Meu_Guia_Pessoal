490aa1c7b0b0f2f728f00dc21b283f17496652ab
bsDuduh <eduardo.santosgit@gmail.com>
2024-08-19 12:08:23 -0300

    css da Tela de login esta completo

--- src/screens/TelaLogin.js
+++ src/screens/TelaLogin.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Image, StyleSheet, Text, TextInput, TouchableOpacity} from 'react-native';
+
+import { View, Image, StyleSheet, Text, TextInput, TouchableOpacity,} from 'react-native';
 import logo from '../assets/Stack_Images/Tela_Login/logo.png';
+import google from '../assets/Stack_Images/Tela_Login/google.png';
 
 export default function TelaLogin(){
     return(
@@ -10,21 +12,36 @@ export default function TelaLogin(){
             
             <Text style={styles.txt_email}>E-MAIL:</Text>
             <TextInput style={styles.email}
-                placeholder = {'Email.exemplo@Gmail.com'}
+                placeholder = {'Email.exemplo@gmail.com'}
             />
 
             <Text style={styles.txt_senha}>SENHA:</Text>
-            <TextInput
-                placeholder = {'*****************'}
+            <TextInput style={styles.senha}
+                placeholder = {'********'}
             />
 
             <TouchableOpacity style={styles.btn_esqueci_senha}>
-                <Text style={styles.esqueci_senha}>esqueci minha senha</Text>
+                <Text style={styles.txt_esqueci_senha}>esqueci minha senha</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.btn_login}>
-                <Text style={styles.login}>LOGIN</Text>
+                <Text style={styles.txt_login}>LOGIN</Text>
             </TouchableOpacity>
+
+            <View style={styles.separador}/>
+
+            <Text style={styles.meios_login}>outros meios de login</Text>
+
+            <TouchableOpacity style={styles.btn_google_login}>
+                <Image source={google} style={styles.img_google}/>
+            </TouchableOpacity>
+            
+            <View style={styles.rowContainer}>
+                <Text style={styles.nao_possui_conta}>não possui uma conta?</Text> 
+                <TouchableOpacity style={styles.btn_criar_conta}>
+                    <Text style={styles.txt_criar_conta}>Crie uma</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -33,28 +50,95 @@ const styles = StyleSheet.create({
     logo: {
         width: 200,
         height: 200,
-        marginTop: 50
     },
     container: {
         flex: 1,
         height: '100%',
         width: '100%',
         alignItems: 'center',
-        padding: 20,
+        padding: 30,
     },
     title: {
         fontSize: 21
     },
     txt_email:{
+        paddingTop: 15,
+        paddingBottom: 10,
         fontSize: 18,
         marginRight: 220
     },
     email:{
-        marginRight: 140
+        paddingLeft: 23,
+        borderWidth: 3,
+        borderColor: '#969696',
+        borderRadius: 50,
+        width: 300,
+        height: 50
     },
     txt_senha:{
+        paddingTop: 20,
+        paddingBottom: 10,
         fontSize: 18,
         marginRight: 220
-    }
+    },
+    senha: {
+       
+        paddingLeft: 23,
+        borderWidth: 3,
+        borderColor: '#969696',
+        borderRadius: 50,
+        width: 300,
+        height: 50
+    },
+    btn_esqueci_senha: {
+        marginRight: 150,
+        paddingTop: 5,
+    },
+    txt_esqueci_senha: {
+        fontSize: 16,
+        color: '#16195D',
+    },
 
+    btn_login: {
+        marginTop: 30,
+        
+        borderRadius: 50,
+        width: 300,
+        height: 50,
+        backgroundColor: '#16195D',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    txt_login: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    separador: {
+        marginVertical: 25, 
+        borderBottomWidth: 1,
+        borderBottomColor: '#969696',  
+        width: 300,
+    },
+    meios_login: {
+        fontSize: 16,
+        color: '#969696'
+    },
+    img_google: {
+        marginTop: 10,
+        width: 70,
+        height: 70
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    nao_possui_conta: {
+        fontSize: 16,
+        color: '#767676'
+    },
+    txt_criar_conta: {
+        color: '#16195D',
+        fontSize: 16,
+        padding: 10,
+    }
 })
