b1187bc77dff696dba022b0335226d6677af435b
Davi Varella <davivarelladev@gmail.com>
2024-09-02 23:43:46 -0300

    modificaçoes
    Relacionadas a style da tela Cadastro organizadores, modificaçao dos nomes dos arquivos / alteraçao do texto introdutorio introduction_Screen

--- Mgp_Front/App.js
+++ Mgp_Front/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
-import Organizador_Screen from './src/screens/login_Screen/organizador_Screen.js';
-
+import Organizador_Screen from './src/screens/Organizador_Screen/Organizador_Screen.js'
+import Introduction_Screen from './src/screens/Introduction_Screen/Introduction_Screen.js'
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#4A4DA1"/>
-      <Organizador_Screen/>
+      {/* <Organizador_Screen/> */}
+      <Introduction_Screen/>
     </View>
   );
 }
