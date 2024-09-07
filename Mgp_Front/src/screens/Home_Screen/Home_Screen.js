91b74c33378fba2be4134887523e889247dc7359
Davi Varella <davivarelladev@gmail.com>
2024-09-07 13:28:35 -0300

    inicio menu hamburguer
    menu hamburguer é dependente de rotas

--- Mgp_Front/src/screens/Home_Screen/Home_Screen.js
+++ Mgp_Front/src/screens/Home_Screen/Home_Screen.js
@@ -1,6 +1,17 @@
+import { View , Text, ScrollView} from "react-native";
+
 export default function Home_Screen(){
     return(
-        <>
-        </>
-    )
+        <ScrollView>
+            <View>
+                <Text>Menu Hamburguer</Text>
+            </View>
+            <View>
+                <Text>Mapa</Text>
+            </View>
+            <View>
+                <Text>Pesquisa</Text>
+            </View>
+        </ScrollView>
+        )
 }
