70f4e3bc6abfc8b254b1aae7b2f8a11768469861
Miguel Freitas <miguelfreitas15012503@gmail.com>
2024-09-30 12:33:32 -0300

    comecei a fazer o estilo da tela responsavel por enviar um email com codigo de verifivação, falta terminar as funcionalidades e o estilo

--- Mgp_Front/App.js
+++ Mgp_Front/App.js
@@ -3,12 +3,13 @@ import { View, StyleSheet, StatusBar } from 'react-native';
 import Organizador_Screen from './src/screens/Organizador_Screen/Organizador_Screen.js'
 import Introduction_Screen from './src/screens/Introduction_Screen/Introduction_Screen.js'
 import Cadastro_Cliente_Screen from './src/screens/Cadastro_Cliente_Screen/Cadastro_Cliente_Screen.js'
+import Send_Email_Screen from './src/screens/Send_Email_Screen/Send_Email_Screen.js';
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#4A4DA1"/>
     {/*   <Cadastro_Cliente_Screen/> */}
-      <Organizador_Screen/>
+      <Send_Email_Screen/>
       {/* <Introduction_Screen/> */}
     </View>
   );
