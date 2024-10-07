ef29fdeefd9fec84665f012f3455a68de6d9fdf7
end <endy.kaori.yara@gmail.com>
2024-10-07 12:23:55 -0300

    07.10.24 Loguin Funcional e LocalRoutes, Model, Cadastro e Listagem adicionadas

--- Mgp_Back/server.js
+++ Mgp_Back/server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const clientRoutes = require('./routes/clientRoutes');
 const organizerRoutes = require('./routes/organizerRoutes');
+const pontoRoutes = require('./routes/pontoRoutes');
 
 const { authenticateClient } = require('./models/clientModel');
 const { authenticateOrganizer } = require('./models/organizerModel');
@@ -15,7 +17,7 @@ app.use(cors());
 
 app.use('/api', clientRoutes);
 app.use('/api', organizerRoutes);
-
+app.use('/api', pontoRoutes);
 
 app.post('/api/login', async (req, res) => {
   const { email, senha } = req.body;
@@ -41,6 +43,52 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
+const config = require('./config/dbconfig');
+const sql = require('mssql');
+
+
+// Dentro do app:
+// const fetchPontosTuristicos = async () => {
+//   try {
+//       const response = await fetch(`http://localhost:3001/listar?max=${max}&min=${min}`);
+
+//       if (response.ok) {
+//           const data = await response.json(); // Parse JSON response
+//           setPontosTuristicos(prev => [...prev, ...data]); // Append new data
+//           setMax(max + 5); // Increase max for next fetch
+//           setMin(min + 5); // Increase min for next fetch
+//       } else {
+//           console.error('Failed to fetch data:', response.statusText);
+//       }
+//   } catch (error) {
+//       console.error('Erro ao buscar pontos turísticos:', error);
+//   }
+// };
+
+app.get('/listar', async (req, res) => {
+  try {
+    await sql.connect(config);
+    
+    // Extracting the query parameters
+    const max = parseInt(req.query.max, 10); // Parse as integer
+    const min = parseInt(req.query.min, 10); // Parse as integer
+
+    const request = new sql.Request();
+    let query = 'SELECT * FROM Pontos_Turisticos WHERE ptour_id =< @max AND ptour_id >= @min';
+
+    request.input('max', sql.BigInt, max);
+    request.input('min', sql.BigInt, min);
+  
+  
+      let result = await request.query(query);
+      res.send(result.recordset);
+  } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).send('Error executing query');
+  }finally {
+    sql.close();
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
